20ea207f4fa74107393ee7af7a5e7a2ca27698c3
Jessie Crider (CriderGPT) <jessiecrider3@gmail.com>
2026-04-05 17:20:13 -0400

    Refactor MediaWindow initialization and button actions

--- CustomMediaPlayer.lua
+++ CustomMediaPlayer.lua
@@ -1,71 +1,79 @@
 -- FFA Themed Media Player Mod for FS25
 -- Author: Jessie Crider
--- Features: Spotify + CriderGPT + Draggable + Minimize
 
 MediaWindow = {}
 MediaWindow.isVisible = false
 MediaWindow.isMinimized = false
 MediaWindow.lastPosition = {x = nil, y = nil}
 
--- FFA Theme Colors
+-- FFA Theme Colors (RGBA)
 FFATheme = {
     bgColor         = {0.10, 0.10, 0.11, 1.0},
-    spotify         = {0.11, 0.73, 0.33, 1.0},
-    cridergpt       = {0.94, 0.65, 0.00, 1.0},
-    closeBtn        = {1.00, 0.30, 0.30, 1.0},
+    spotify         = {0.11, 0.73, 0.33, 1.0},   -- Spotify green
+    cridergpt       = {0.94, 0.65, 0.00, 1.0},   -- CriderGPT orange
+    closeBtn        = {1.00, 0.33, 0.33, 1.0},
     minimizeBtn     = {1.00, 0.80, 0.40, 1.0},
-    inactiveBtn     = {0.65, 0.65, 0.70, 1.0},
-    hoverBright     = {1.0, 1.0, 1.0, 1.0}
+    inactiveBtn     = {0.65, 0.65, 0.70, 1.0}
 }
 
 function MediaWindow:init()
-    if self.window ~= nil then return end
+    if self.window ~= nil then 
+        return 
+    end
 
     self.window = g_gui:loadGui("customMediaPlayer.xml", "CustomMediaPlayer", self)
 
     if self.window == nil then
-        print("Error: Could not load customMediaPlayer.xml")
+        print("FFA Media Player: Failed to load customMediaPlayer.xml")
         return
     end
 
     self.window:setVisible(false)
 
-    -- Find elements
-    self.mainFrame     = self.window:find("MainFrame")
-    self.bg            = self.window:find("MediaBackground")
-    self.webView       = self.window:find("MediaWebView")
-    self.spotifyBtn    = self.window:find("SpotifyButton")
-    self.criderBtn     = self.window:find("CriderGPTButton")
-    self.closeBtn      = self.window:find("CloseButton")
-    self.minimizeBtn   = self.window:find("MinimizeButton")
-    self.titleBar      = self.window:find("TitleBar")
+    -- Find UI elements
+    self.mainFrame   = self.window:find("MainFrame")
+    self.bg          = self.window:find("MediaBackground")
+    self.webView     = self.window:find("MediaWebView")
+    self.spotifyBtn  = self.window:find("SpotifyButton")
+    self.criderBtn   = self.window:find("CriderGPTButton")
+    self.closeBtn    = self.window:find("CloseButton")
+    self.minimizeBtn = self.window:find("MinimizeButton")
+    self.titleBar    = self.window:find("TitleBar")
 
     if self.bg then
         self.bg:setColor(unpack(FFATheme.bgColor))
     end
 
-    -- Button actions
+    -- Button callbacks
     if self.spotifyBtn then
         self.spotifyBtn.onClick = function()
-            if self.webView then self.webView:setUrl("https://open.spotify.com/") end
+            if self.webView then 
+                self.webView:setUrl("https://open.spotify.com/") 
+            end
             self:highlightButton("spotify")
         end
     end
 
     if self.criderBtn then
         self.criderBtn.onClick = function()
-            if self.webView then self.webView:setUrl("https://cridergpt.lovable.app/") end
+            if self.webView then 
+                self.webView:setUrl("https://cridergpt.lovable.app/") 
+            end
             self:highlightButton("cridergpt")
         end
     end
 
     if self.closeBtn then
-        self.closeBtn.onClick = function() self:setVisible(false) end
+        self.closeBtn.onClick = function()
+            self:setVisible(false)
+        end
         self.closeBtn:setColor(unpack(FFATheme.closeBtn))
     end
 
     if self.minimizeBtn then
-        self.minimizeBtn.onClick = function() self:minimize() end
+        self.minimizeBtn.onClick = function()
+            self:minimize()
+        end
         self.minimizeBtn:setColor(unpack(FFATheme.minimizeBtn))
     end
 
@@ -75,7 +83,7 @@ function MediaWindow:init()
         self:highlightButton("spotify")
     end
 
-    print("FFA Media Player ready! Press F9 to open.")
+    print("FFA Media Player initialized successfully! Press F9 to open.")
 end
 
 function MediaWindow:highlightButton(active)
@@ -90,7 +98,8 @@ end
 function MediaWindow:minimize()
     if self.mainFrame and self.isVisible then
         self.isMinimized = true
-        self.lastPosition = {x = self.mainFrame:getPositionX(), y = self.mainFrame:getPositionY()}
+        self.lastPosition.x = self.mainFrame:getPositionX()
+        self.lastPosition.y = self.mainFrame:getPositionY()
         self:setVisible(false)
     end
 end
@@ -99,7 +108,7 @@ function MediaWindow:toggle()
     if self.isMinimized then
         self.isMinimized = false
         self:setVisible(true)
-        if self.lastPosition.x then
+        if self.lastPosition.x and self.mainFrame then
             self.mainFrame:setPosition(self.lastPosition.x, self.lastPosition.y)
         end
     else
@@ -115,8 +124,8 @@ function MediaWindow:setVisible(state)
     end
 end
 
--- Key handling
-function MediaWindow:keyEvent(unicode, key, down, shift, ctrl, alt)
+-- Key event (F9)
+function MediaWindow:keyEvent(unicode, key, down)
     if down and key == Input.KEY_F9 then
         self:toggle()
     end
@@ -125,7 +134,7 @@ end
 -- Initialize
 MediaWindow:init()
 
--- Register key
+-- Register F9 key
 if g_inputBinding then
     g_inputBinding:registerActionEvent(InputAction.MENU, MediaWindow, MediaWindow.keyEvent, false, true, false, true)
 end
